67c90cb72fed98843dc9b1d33e51a3aa0d27a1ae
Lisa <llvvss@icloud.com>
2024-03-12 12:38:42 -0600

    again, Readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# whileloop
+Write out all the numbers between 100 to 500 that are divisible by 3 and 5.  
+Uses a while loop
--- Source Code/whileloop.cpp
+++ Source Code/whileloop.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main()
 {
-    
+    // Write out all the numbers between 100 to 500 that are divisible by 3 and 5.
+
+    int counter = 100;
+
+    while (counter <= 500)
+    {
+        if (counter % 3 == 0 && counter % 5 == 0)
+        {
+            cout << counter << " is divisible by both 3 and 5\n";
+        }
+        counter++;
+    }
+
 }
